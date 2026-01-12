5add20457cfb544a74d9ca219ab2075ec262dc59
Jsplix <jkjin112@gmail.com>
2026-01-12 23:44:08 +0900

    chore: project settings

---
+++ src/main/java/kr/wayout/wayout/WayoutApplication.java
@@ -0,0 +1,13 @@
+package kr.wayout.wayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/wayout/wayout/WayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wayout.wayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
