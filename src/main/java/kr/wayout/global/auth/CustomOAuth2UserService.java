98b9778df4f033f64b3bf52add3022f04a2a0c83
Jsplix <jkjin112@gmail.com>
2026-01-25 02:33:52 +0900

    feat: oauth 로그인을 위한 기능 구현

---
+++ src/main/java/kr/wayout/global/auth/CustomOAuth2UserService.java
@@ -0,0 +1,32 @@
+package kr.wayout.global.auth;
+
+import kr.wayout.domain.member.Member;
+import kr.wayout.domain.member.MemberService;
+import kr.wayout.domain.member.dto.OAuth2UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberService memberService;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String email = (String) attributes.get("email");
+
+        Member member = memberService.readOrCreate(email);
+
+        return new OAuth2UserInfo(member, attributes);
+    }
+}
