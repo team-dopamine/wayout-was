37951a23f64e134705ac7d99ebae38b63dbd7a65
Jsplix <jkjin112@gmail.com>
2026-01-27 00:22:34 +0900

    feat: refresh token을 store에 저장하는 로직 추가

--- src/main/java/kr/wayout/global/auth/OAuth2SuccessHandler.java
+++ src/main/java/kr/wayout/global/auth/OAuth2SuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import kr.wayout.domain.member.Member;
 import kr.wayout.domain.member.dto.OAuth2UserInfo;
 import kr.wayout.global.auth.jwt.JwtProvider;
+import kr.wayout.global.auth.jwt.RefreshTokenStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -19,6 +20,7 @@ import java.io.IOException;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtProvider jwtProvider;
+    private final RefreshTokenStore refreshTokenStore;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -28,6 +30,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String accessToken = jwtProvider.createAccessToken(member.getEmail(), member.getRole());
         String refreshToken = jwtProvider.createRefreshToken(member.getEmail(), member.getRole());
         boolean isNewMember = member.getNickname().startsWith("USER_");
+
+        refreshTokenStore.save(member.getEmail(), refreshToken);
 
         Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
         accessTokenCookie.setPath("/");
