136a3ba38ca0858c3a9e445a260f3a605e4e84c1
Jsplix <jkjin112@gmail.com>
2026-02-02 23:50:36 +0900

    fix: jwt 토큰 관리 방식 변경으로 인한 로직 수정

--- src/main/java/kr/wayout/global/auth/controller/AuthController.java
+++ src/main/java/kr/wayout/global/auth/controller/AuthController.java
@@ -1,13 +1,12 @@
 package kr.wayout.global.auth.controller;
 
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import kr.wayout.global.auth.OAuth2UserInfo;
 import kr.wayout.global.auth.AuthService;
 import kr.wayout.global.auth.dto.SignOutDto;
 import kr.wayout.global.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,25 +23,27 @@ public class AuthController implements AuthApi {
     private final AuthService authService;
     private final JwtProvider jwtProvider;
 
+    @Value("${app.cookie-domain}")
+    private String frontendUrl;
+
     @Override
     @PostMapping("/sign-out")
     public ResponseEntity<?> signOut(@AuthenticationPrincipal String email, HttpServletResponse response) {
         authService.signOut(email);
-        // TODO: Nginx HTTPS 설정 후 secure 부분 주석 해제
-        // Refresh Token 쿠키 삭제
         ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", "")
                 .path("/")
+                .domain(frontendUrl)
                 .httpOnly(true)
-//                .secure(true)
+                .secure(true)
                 .sameSite("Lax")
                 .maxAge(0)
                 .build();
         response.addHeader("Set-Cookie", refreshCookie.toString());
 
-        // Access Token 쿠키 삭제
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", "")
                 .path("/")
-//                .secure(true)
+                .domain(frontendUrl)
+                .secure(true)
                 .sameSite("Lax")
                 .maxAge(0)
                 .build();
@@ -58,7 +59,8 @@ public class AuthController implements AuthApi {
         // TODO: Nginx HTTPS 설정 후 secure 부분 주석 해제
         ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", newAccessToken)
                 .path("/")
-//                .secure(true)
+                .domain(frontendUrl)
+                .secure(true)
                 .sameSite("Lax")
                 .maxAge(jwtProvider.getExpirationTime(newAccessToken) / 1000)
                 .build();
