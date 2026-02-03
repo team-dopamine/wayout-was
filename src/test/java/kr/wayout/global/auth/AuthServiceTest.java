ce35f6b5c30f60a07970a50f1bc7261afd3ed4d1
Jsplix <jkjin112@gmail.com>
2026-02-04 01:51:24 +0900

    test: 회원 탈퇴 api 개발을 위한 테스트 코드 작성

--- src/test/java/kr/wayout/global/auth/AuthServiceTest.java
+++ src/test/java/kr/wayout/global/auth/AuthServiceTest.java
@@ -1,11 +1,12 @@
 package kr.wayout.global.auth;
 
+import kr.wayout.domain.member.Member;
+import kr.wayout.domain.member.MemberService;
 import kr.wayout.domain.member.Role;
 import kr.wayout.global.auth.jwt.JwtProvider;
 import kr.wayout.global.auth.jwt.RefreshTokenStore;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -13,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +27,9 @@ class AuthServiceTest {
 
     @Mock
     private RefreshTokenStore refreshTokenStore;
+
+    @Mock
+    private MemberService memberService;
 
     @Test
     @DisplayName("유효한 Refresh Token으로 Access Token 재발급 성공")
@@ -81,7 +84,7 @@ class AuthServiceTest {
 
     @Test
     @DisplayName("로그아웃 시 RefreshTokenStore에서 토큰 삭제")
-    void signOut_Success() {
+    void signOut_success() {
         // given
         String email = "test@example.com";
 
@@ -90,5 +93,24 @@ class AuthServiceTest {
 
         // then
         BDDMockito.verify(refreshTokenStore).delete(email);
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴 API - 성공 테스트")
+    void withdraw_success() {
+        // given
+        String email = "test@gmail.com";
+        Member member = Member.builder()
+                .email(email)
+                .role(Role.USER)
+                .build();
+
+        BDDMockito.given(memberService.read(email)).willReturn(member);
+
+        // when
+        String result = authService.withdraw(email);
+
+        // then
+        Assertions.assertThat(result).isEqualTo("탈퇴 처리가 완료되었습니다.");
     }
 }
