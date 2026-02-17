75e346149a345d30cbe3a8760f19776ce6418b92
Jsplix <jkjin112@gmail.com>
2026-02-17 23:02:49 +0900

    feat: problem 연관관계 설정을 위한 jpa repository 추가

---
+++ src/main/java/kr/wayout/domain/problem/ProblemRepository.java
@@ -0,0 +1,8 @@
+package kr.wayout.domain.problem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
+}
