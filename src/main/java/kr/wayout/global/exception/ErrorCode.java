05fd308394e0bcf2113c8c4d1596d6097456a41f
Jsplix <jkjin112@gmail.com>
2026-03-06 09:26:39 +0900

    fix: 에러 코드 중복 제거

--- src/main/java/kr/wayout/global/exception/ErrorCode.java
+++ src/main/java/kr/wayout/global/exception/ErrorCode.java
@@ -17,7 +17,7 @@ public enum ErrorCode {
     // B: 비즈니스 로직 오류
     MEMBER_NOT_FOUND("B001", HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     PROBLEM_NOT_FOUND("B002", HttpStatus.NOT_FOUND, "존재하지 않는 문제입니다."),
-    MEMBER_ALREADY_DELETED("B001", HttpStatus.CONFLICT, "이미 삭제 된 사용자입니다."),
+    MEMBER_ALREADY_DELETED("B003", HttpStatus.CONFLICT, "이미 삭제 된 사용자입니다."),
 
     // V: 요청 관련 오류
     INVALID_VALUE("V001", HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
