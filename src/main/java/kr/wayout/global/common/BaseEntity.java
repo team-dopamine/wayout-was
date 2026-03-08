5d9ce29a334572417eb18aa67c5dc59272ffb362
Jsplix <jkjin112@gmail.com>
2026-03-08 22:20:44 +0900

    feat: 제출 시간을 조회하기 위한 getter 추가

--- src/main/java/kr/wayout/global/common/BaseEntity.java
+++ src/main/java/kr/wayout/global/common/BaseEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.SQLDelete;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @SQLDelete(sql = "UPDATE #{entityName} SET deleted_at = NOW() WHERE id = ?")
